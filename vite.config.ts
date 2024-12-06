0325f20a54d76e808d2c973c505e664f7b2e2cee
Malajka365 <59540874+Malajka365@users.noreply.github.com>
2024-12-06 12:53:33 +0100

    deploy??
    github public proba vissza

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Video-Gallery-V7/',
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-      exclude: [
-        'node_modules/',
-        'src/setupTests.ts',
-      ],
-    },
-  },
 });
